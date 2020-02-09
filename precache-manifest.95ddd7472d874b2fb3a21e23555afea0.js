0020ed8eaf06d850bc2ecd62e4f7f8f8a8a16874
Saurabh Burade <sburade3@gmail.com>
2020-02-09 14:25:47 +0530

    Updates

---
+++ precache-manifest.95ddd7472d874b2fb3a21e23555afea0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ebc6acb416d64aa8bae6384b83c4d22",
+    "url": "saurabhburade.github.io/React-Todo-App/index.html"
+  },
+  {
+    "revision": "e7fda94303e43d321040",
+    "url": "saurabhburade.github.io/React-Todo-App/static/css/main.1cf1f38e.chunk.css"
+  },
+  {
+    "revision": "0a0e5626af8720d24129",
+    "url": "saurabhburade.github.io/React-Todo-App/static/js/2.2fd1f3f6.chunk.js"
+  },
+  {
+    "revision": "e7fda94303e43d321040",
+    "url": "saurabhburade.github.io/React-Todo-App/static/js/main.19c84a47.chunk.js"
+  },
+  {
+    "revision": "ecf2d520f3da503ee3a7",
+    "url": "saurabhburade.github.io/React-Todo-App/static/js/runtime~main.bda2c57a.js"
+  }
+]);
